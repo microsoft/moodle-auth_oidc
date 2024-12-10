microsoft/o365-moodle

Handle empty binding username claim configurations

auth/oidc/classes/form/binding_username_claim.php
@@ -116,7 +116,7 @@ class binding_username_claim extends moodleform {
         $mform->addElement(
                 'select',
                 'bindingusernameclaim',
-                auth_oidc_config_name_in_form('binding_username_claim'),
+                auth_oidc_config_name_in_form('beingusernameclaim'),
                 $bindingusernameoptions
         );
         $mform->setDefault('bindingusernameclaim', 'auto');

microsoft/moodle-auth_oidc

classes/form/binding_username_claim.php
@@ -116,7 +116,7 @@ class binding_username_claim extends moodleform {
         $mform->addElement(
                 'select',
                 'bindingusernameclaim',
-                auth_oidc_config_name_in_form('binding_username_claim'),
+                auth_oidc_config_name_in_form('bindingusernameclaim'),
                 $bindingusernameoptions
         );
         $mform->setDefault('bindingusernameclaim', 'auto');